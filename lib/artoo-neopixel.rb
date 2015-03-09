1ccff3c9b44c336195e4128b4276046a812f0ae4
Jay Hayes <ur@iamvery.com>
2015-03-09 18:13:41 -0500

    Require all the extensions when gem is required

---
+++ lib/artoo-neopixel.rb
@@ -0,0 +1,1 @@
+require "artoo-neopixel/extensions"
---
+++ lib/artoo-neopixel/extensions.rb
@@ -0,0 +1,7 @@
+require "artoo/adaptors/firmata"
+require "firmata/board"
+require "firmata/constants"
+
+require "artoo-neopixel/extensions/artoo/adaptors/firmata"
+require "artoo-neopixel/extensions/firmata/board"
+require "artoo-neopixel/extensions/firmata/constants"
