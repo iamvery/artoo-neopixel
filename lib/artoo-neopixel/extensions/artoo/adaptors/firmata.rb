c199f726000c8f34e4e1ddc14c575455a5f27fea
Jay Hayes <ur@iamvery.com>
2015-03-10 18:12:57 -0500

    Add corresponding register methods to firmata adaptor

--- lib/artoo-neopixel/extensions/artoo/adaptors/firmata.rb
+++ lib/artoo-neopixel/extensions/artoo/adaptors/firmata.rb
@@ -1,8 +1,16 @@
 module Artoo
   module Adaptors
     class Firmata
+      def register_neopixel(pin, count)
+        firmata.register_neopixel(pin, count)
+      end
+
       def neopixel(index, red, green, blue)
         firmata.neopixel(index, red, green, blue)
+      end
+
+      def register_matrix(pin, width, height)
+        firmata.register_neomatrix(pin, width, height)
       end
 
       def neomatrix(x, y, red, green, blue)
