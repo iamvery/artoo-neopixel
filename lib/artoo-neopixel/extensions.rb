3941482138568cec6991dc45ce2ba2ce5ed18310
Jay Hayes <ur@iamvery.com>
2015-03-10 18:11:25 -0500

    Require entire firmata lib
    * This dependency isn't laid out modularly, so we can't get away with
      just requiring the pieces we need. No big deal really.

--- lib/artoo-neopixel/extensions.rb
+++ lib/artoo-neopixel/extensions.rb
@@ -1,6 +1,5 @@
 require "artoo/adaptors/firmata"
-require "firmata/board"
-require "firmata/constants"
+require "firmata"
 
 require "artoo-neopixel/extensions/artoo/adaptors/firmata"
 require "artoo-neopixel/extensions/firmata/board"
